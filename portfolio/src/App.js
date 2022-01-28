d130f78c0b8867199b89d5a16c3334812d79323f
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2022-01-28 15:53:15 +0100

    First file structure, implement MenueBar.js

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+// In App.js everything comes together form components and/or pages;
+import MenueBar from "./components/MenueBar";
+import "./index.css";
+
+function App() {
+  return (
+    // these are fragments <> + </>
+    <>
+      <MenueBar />
+    </>
+  );
+}
+
+export default App;
